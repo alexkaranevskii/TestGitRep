73dc4eaf2094ba480e5b92550ce64dee729430bb
alexkaranevskii <alexkaranevskii@mail.ru>
2019-10-03 12:23:00 +0300

    В объекте класса Dog dog полю beast присвоено название Овчарка

--- java/001/src/UROK1/MainClass.java
+++ java/001/src/UROK1/MainClass.java
@@ -4,7 +4,8 @@ public class MainClass {
     public static void main(String[] args) {
         Dog labrador = new Dog();
         labrador.name ="Шарик";
+        labrador.beast ="Овчарка";
 
-        System.out.println( labrador.name);
+        System.out.println( labrador.name +" "+ labrador.beast);
     }
 }
