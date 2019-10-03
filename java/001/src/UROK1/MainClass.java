8b050437c5db66b23b241a9e5ec3683ac91875ea
alexkaranevskii <alexkaranevskii@mail.ru>
2019-10-03 14:52:14 +0300

    Создан объект класса Dog bolonka

--- java/001/src/UROK1/MainClass.java
+++ java/001/src/UROK1/MainClass.java
@@ -8,5 +8,12 @@ public class MainClass {
         labrador.color ="бежевый";
         labrador.age = 7;
         System.out.println(labrador.beast+ " " + labrador.name + " цвет " + labrador.color + " " + labrador.age +" лет ");
+
+        Dog bolonka = new Dog();
+        bolonka.name ="Тиша";
+        bolonka.beast ="Болонка";
+        bolonka.color ="белый";
+        bolonka.age = 5;
+        System.out.println(bolonka.beast+ " " + bolonka.name + " цвет " + bolonka.color + " " + bolonka.age +" лет ");
     }
 }
