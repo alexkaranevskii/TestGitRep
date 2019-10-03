da2db4680df9c61e40e439b03b53f86e2ac9c41a
alexkaranevskii <alexkaranevskii@mail.ru>
2019-10-03 12:50:29 +0300

    Что то

--- java/001/src/UROK1/MainClass.java
+++ java/001/src/UROK1/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
     public static void main(String[] args) {
         Dog labrador = new Dog();
         labrador.name ="Шарик";
-
+        labrador.beast ="Лабрадор";
         System.out.println( labrador.name);
     }
 }
