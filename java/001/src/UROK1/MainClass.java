5c0dd1e4e49f68664f022f7bc25bc7c6986e31b3
alexkaranevskii <alexkaranevskii@mail.ru>
2019-10-03 12:19:50 +0300

    Создан объект класса Dog dog Полю name объекта dog пррисвоено имя Шарик

--- java/001/src/UROK1/MainClass.java
+++ java/001/src/UROK1/MainClass.java
@@ -3,5 +3,8 @@ package UROK1;
 public class MainClass {
     public static void main(String[] args) {
         Dog labrador = new Dog();
+        labrador.name ="Шарик";
+
+        System.out.println( labrador.name);
     }
 }
