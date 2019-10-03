32259577376a45391ce4cfed67b73bf47d4a410c
alexkaranevskii <alexkaranevskii@mail.ru>
2019-10-03 12:33:02 +0300

    Revert "В объекте класса Dog dog полю beast присвоено название Лабрадор"
    This reverts commit 73dc4eaf2094ba480e5b92550ce64dee729430bb.

--- java/001/src/UROK1/MainClass.java
+++ java/001/src/UROK1/MainClass.java
@@ -4,8 +4,7 @@ public class MainClass {
     public static void main(String[] args) {
         Dog labrador = new Dog();
         labrador.name ="Шарик";
-        labrador.beast ="Овчарка";
 
-        System.out.println( labrador.name +" "+ labrador.beast);
+        System.out.println( labrador.name);
     }
 }
